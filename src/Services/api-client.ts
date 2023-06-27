e552c1db65177687dc43a55d7730f2bfc6a0db1e
ivsheva <sevcenkoivan673@gmail.com>
2023-06-27 12:44:27 +0300

    Implement infinite queries

--- src/Services/api-client.ts
+++ src/Services/api-client.ts
@@ -1,7 +1,8 @@
-import axios, { AxiosError, AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T>{
     count: number;
+    next: string | null;
     results: T[];
 }
 
