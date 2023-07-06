69bdd9e14e0f247085b43a281fa73b46d3a77a41
ivsheva <sevcenkoivan673@gmail.com>
2023-07-06 12:21:02 +0300

    Fetch game details

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -1,14 +1,16 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
 import APIClient, { FetchResponse } from "../Services/api-client";
+import useGameQueryStore from "../store";
 import { Platform } from "./usePlatforms";
-import useGameQueryStore, { GameQuery } from "../store";
 
 const apiClient = new APIClient<Game>("/games");
 
 export interface Game {
   id: number;
   name: string;
+  slug: string;
+  description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/layout";
 import { Outlet } from "react-router-dom";
 import NavBar from "../Components/NavBar/NavBar";
 
@@ -5,7 +6,9 @@ const Layout = () => {
   return (
     <>
       <NavBar />
-      <Outlet />
+      <Box padding={5}>
+        <Outlet />
+      </Box>
     </>
   );
 };
