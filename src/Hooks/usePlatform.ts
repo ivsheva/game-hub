1cca36776e20b4f4b93146d55bad33b097921f84
ivsheva <sevcenkoivan673@gmail.com>
2023-07-10 11:51:33 +0300

    Bug fix

--- src/Hooks/usePlatform.ts
+++ src/Hooks/usePlatform.ts
@@ -1,10 +1,10 @@
 import usePlatforms from "./usePlatforms";
-
+import Platform from "../Entities/Platform";
 
 const usePlatform = (id?: number) => {
     const {data: platforms} = usePlatforms();
 
-    return platforms?.results.find(p => p.id === id)
+    return platforms?.results.find((p: Platform) => p.id === id)
 }
 
 export default usePlatform
